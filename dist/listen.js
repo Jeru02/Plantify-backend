d37d2f97c566dd124f322571fc892ee4dae4d0e7
tplusk <macfelelhun@gmail.com>
2025-06-04 15:19:25 +0100

    fix listen.ts

--- dist/listen.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const api_1 = __importDefault(require("./api"));
-const { PORT = 9090 } = process.env;
-api_1.default.listen(PORT, () => console.log(`Listening on ${PORT}...`));
